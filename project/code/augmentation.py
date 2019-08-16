fc4b0e50c5c514e81a91f32cf36db3157e2caaac
unknown <lior1990@gmail.com>
2019-08-16 18:24:36 +0300

    add augmentation support #2

--- project/code/augmentation.py
+++ project/code/augmentation.py
@@ -1,5 +1,41 @@
-def f(img, label):
-    return img, label
+import random
+
+from torchvision.transforms import ColorJitter, Grayscale, RandomChoice
+from torchvision.transforms.functional import hflip, vflip, rotate
 
 
-augmentation_func = f
+def transform(image, label):
+    image, label = transform_both(image, label)
+    image = transform_image_only(image)
+
+    return image, label
+
+
+def transform_image_only(image):
+    if random.random() > 0.5:
+        # random change the colors of the picture
+        image = RandomChoice([Grayscale(num_output_channels=3), ColorJitter(1, 1, 1)])(image)
+    return image
+
+
+def transform_both(image, label):
+    # Random horizontal flipping
+    if random.random() > 0.5:
+        image = hflip(image)
+        label = hflip(label)
+
+    # Random vertical flipping
+    if random.random() > 0.5:
+        image = vflip(image)
+        label = vflip(label)
+
+    # Random rotation
+    if random.random() > 0.5:
+        angle = random.randint(0, 90)
+        image = rotate(image, angle)
+        label = rotate(label, angle)
+
+    return image, label
+
+
+augmentation_func = transform
